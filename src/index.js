9b237400be588fbbb6e41e9f1ef6d31e73d9706c
ken5scal <kengoscal@gmail.com>
2017-01-12 00:50:59 +0900

    Write Hello World on browser for the first time

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Create a new component. this should produce some html
+// Class. Not an Instance
+// const App = function() {
+const App = () => {
+    // Returning JSx. JSx is tranpiled into vanilla JavaScript.
+    // JSx produces actual html put into DOM when rendering.
+    return <div>Hi!</div>; 
+}
+
+// Take this componet's generated HTML and put it on the page (in the DOM)
+// Instantiating App Class defined in Line 5
+// .container comes from root Container defined in index.html.
+// All component will be nested under here
+ReactDOM.render(<App />, document.querySelector('.container'));
+// ReactDOM.render(<App></App>, );
