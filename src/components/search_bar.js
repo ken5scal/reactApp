89d184928d5d5ccc60ee2a11237ebd0dadf873da
ken5scal <kengoscal@gmail.com>
2017-01-13 08:37:31 +0900

    First event handler in React

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const SearchBar = () => {
-    return <input />  // React.createComment(
-};
+//const SearchBar = () => { // Functional Component
+//    return <input />  // React.createComment(
+//};
+
+//class SearchBar extends React.Component {
+class SearchBar extends Component { // Class Base Component
+    render() {
+       return <input onChange={this.onInputChange} />;  
+    }
+
+    // Event Handler
+    onInputChange(event) {
+        console.log(event.target.value);
+    }
+}
 
 // Exporting SearchBar Component
 export default SearchBar;
