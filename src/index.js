0bf9b2cfa5a6e4165b542cb35ea9321be95f6e5b
ken5scal <kengoscal@gmail.com>
2017-01-14 12:07:28 +0900

    Add search API

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyDzHKm8wBzdCBq7RDrvjwFGB0Lvwo0e_iU'
+
+YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
+    console.log(data);
+});
 
 // Create a new component. this should produce some html
 // Class. Not an Instance
